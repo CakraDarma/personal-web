d81af6bfedf4260337914b811b0ade56d01c306f
CakraDarma <baguscakraa02@gmail.com>
2023-06-24 13:46:20 +0800

    delete env.mjs

--- env.mjs
+++ env.mjs
@@ -5,14 +5,14 @@ export const env = createEnv({
   server: {
     // This is optional because it's only used in development.
     // See https://next-auth.js.org/deployment.
-    NEXTAUTH_URL: z.string().url().optional(),
-    NEXTAUTH_SECRET: z.string().min(1),
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_CLIENT_SECRET: z.string().min(1),
-    GITHUB_ACCESS_TOKEN: z.string().min(1),
-    GOOGLE_CLIENT_ID: z.string().min(1),
-    GOOGLE_CLIENT_SECRET: z.string().min(1),
-    DATABASE_URL: z.string().min(1),
+    // NEXTAUTH_URL: z.string().url().optional(),
+    // NEXTAUTH_SECRET: z.string().min(1),
+    // GITHUB_CLIENT_ID: z.string().min(1),
+    // GITHUB_CLIENT_SECRET: z.string().min(1),
+    // GITHUB_ACCESS_TOKEN: z.string().min(1),
+    // GOOGLE_CLIENT_ID: z.string().min(1),
+    // GOOGLE_CLIENT_SECRET: z.string().min(1),
+    // DATABASE_URL: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
